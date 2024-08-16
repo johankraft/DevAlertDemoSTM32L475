ab970641fdc493c59550fb7ae172cae58b3fdfc1
johankraft <johan.kraft@percepio.com>
2024-08-16 09:59:28 +0200

    * Added bat file for Eval/Sandbox upload.

---
+++ DevAlertDemo/HostTools/da-serial-eval.bat
@@ -0,0 +1,13 @@
+
+rem Example script for uploading DevAlert data from a serial terminal log to a DevAlert evaluation account
+echo off
+
+rem Update these paths to match your project build and file system.
+set CURRENT_ELF_FILE="C:\src\github-repos\DevAlertDemoSTM32L475-May24\Debug-Serial\aws_demos.elf"
+set ELF_ARCHIVE_FOLDER="C:\Users\johan\Percepio AB Dropbox\Johan Kraft\DvA-ELF-archive"
+
+rem Copy elf file and rename it using the GCC Build ID.
+python store-elf.py %CURRENT_ELF_FILE% %ELF_ARCHIVE_FOLDER%
+
+rem Parse all text that is written to the log file, extract the DevAlert data and pass it to devalerthttps for upload
+python devalertserial.py --upload sandbox teraterm.log | devalerthttps.exe store-trace
